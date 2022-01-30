8f847ec0477c1319ec33fab48550816a65d67bdb
marciofao <marcio.lopes.fao@gmail.com>
2022-01-30 20:51:43 -0300

    Initialize

---
+++ member-id-card.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @wordpress-plugin
+ * Plugin Name: Member ID Card
+ * Plugin URI: http://opah.com.br
+ * Description: Adds a ID card link to user info page
+ * Version: 1.0
+ * Text Domain: user-id-card
+ * Author: Márcio Lopes Fão
+ * Author URI: https://marciofao.github.io/
+
+ */
+
+
+
+ 
