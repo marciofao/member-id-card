41d468103aea8ef59fad8bc762c34a18ced76ba0
marciofao <marcio.lopes.fao@gmail.com>
2022-06-21 15:59:06 -0300

    aria label acessibility

--- card-page.php
+++ card-page.php
@@ -34,7 +34,7 @@ $usr = get_user_by('id', base64_decode($_GET['card-view']));
             <h2><span class="display-name"><?php echo $usr->display_name ?> </span></h2>
             <?php $profile_pic = get_user_meta($usr->ID, 'simple_local_avatar', true)['full'] ?>
                 <?php if (!$profile_pic) $profile_pic = MIC_PLUGIN_URI . 'img/blank_profile.png' ?>
-            <div class="picture" style="background: url('<?php echo $profile_pic ?>')">
+            <div class="picture" style="background: url('<?php echo $profile_pic ?>')" role="img" aria-label="<?php _e('User Picture', 'mic') ?>">
                
             </div>
         </div>
